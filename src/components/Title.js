74d5f855a16adf0bc295254392741b543abffc70
Austin Luu <32989647+AustinLuu@users.noreply.github.com>
2022-01-19 13:08:36 -0500

    detailing

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>image-repository-shopify</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Photobook</h1>
+      <h2>Austin's Gallery</h2>
+      <p>Welcome to my little photobook!</p>
     </div>
   )
 }
